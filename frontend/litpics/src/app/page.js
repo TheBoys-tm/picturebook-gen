bd0222833ccbb722dcdef37998a32c7975ef946a
UBC Student <efedemir2001@gmail.com>
2023-05-14 16:54:56 -0700

    kia

--- frontend/litpics/src/app/page.js
+++ frontend/litpics/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       {/* Left card with image display */}
       <div className={styles.card}>
-        <img src="/app/static/kia.png" alt="Placeholder Kia" />
+      <img src="https://media.licdn.com/dms/image/D5603AQEYoIjdm-9o5g/profile-displayphoto-shrink_800_800/0/1676057663943?e=2147483647&v=beta&t=ixDdpyLFs3U9dWylfL2Z-1gc-6C4FSFiK5FrgdHFzWE" alt="Placeholder Kia" />
       </div>
 
       {/* Right card with text area */}
