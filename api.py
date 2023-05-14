cc6c80e6af0695b2688b6d01d0ec1127f4802714
shreymittal1000 <shreymittal1000@gmail.com>
2023-05-14 23:37:05 +0200

    buffer commit

--- api.py
+++ api.py
@@ -1,14 +1,29 @@
-import flask
-import gpt4
+import openai
+import time
+from dotenv import load_dotenv
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    if flask.request.method == 'GET':
-        return(flask.render_template('index.html'))
-    if flask.request.method == 'POST':
-        query = flask.request.form['query']
-        response = gpt4(query)
-        return flask.render_template('index.html', query=query, response=response)
-    
-if __name__ == '__main__':
-    app.run(debug=True)
+load_dotenv()
+
+openai.api_key = OPENAI_API_KEY
+openai.organization = OPENAI_ORG_KEY
+
+def make_query(query: str):
+    """
+    Description:
+        Make query to OpenAI GPT4
+    Args:
+        query (str): Query to be made to GPT4
+    Returns:
+        str: Response from GPT4"""
+    reso = openai.Completion.create(
+        engine="davinci",
+        prompt=query,
+        temperature=0.9,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n", "Human:", "AI:"]
+    )
+    return reso.choices[0].text
+
