33fda0123629f1aa009c830e4e95b19655f3a4de
Victor Ivanov <vivanov@paradigmtesting.com>
2023-05-15 11:09:20 -0400

    added test files, prompt works and gives link -V

--- api.py
+++ api.py
@@ -1,13 +1,15 @@
 import openai
 import os
 from dotenv import load_dotenv
+import requests
+import json
 
 load_dotenv(".env")
 
 openai.api_key = os.environ["GPT4_API_KEY"]
 openai.organization = os.environ["GPT4_ORG_ID"]
 
-def make_query(query: str) -> str:
+def get_main_text(query: str) -> str:
     """
     Description:
         Make query to OpenAI GPT4
@@ -16,17 +18,39 @@ def make_query(query: str) -> str:
     Returns:
         str: Response from GPT4
     """
-    reso = openai.Completion.create(
-        engine="davinci",
-        prompt=query,
-        temperature=0.9,
-        max_tokens=100,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        stop=["\n", "Human:", "AI:"]
-    )
-    print(reso.choices[0])
-    return reso.choices[0].text
+    response = openai.ChatCompletion.create( model="gpt-4", messages=[
+        {"role": "system", "content": "You are a describer ai, you will take a whole page of text and take the most important part of it. Then in your own way describe the scene like you would a scene from a book, try to keep it to 1 sentence but you can go to 2 if needed."},
+        {"role": "user", "content": query}
+    ])
+    return response['choices'][0]['message']['content']
 
-make_query("Why is the sky blue?")
+def text_to_SD(text: str) -> str:
+    url = "https://stablediffusionapi.com/api/v3/text2img"
+
+    payload = json.dumps({
+    "key": os.environ["SD_API_KEY"],
+    "prompt": text,
+    "negative_prompt": None,
+    "width": "512",
+    "height": "512",
+    "samples": "1",
+    "num_inference_steps": "20",
+    "seed": None,
+    "guidance_scale": 7.5,
+    "safety_checker": "yes",
+    "multi_lingual": "no",
+    "panorama": "no",
+    "self_attention": "no",
+    "upscale": "no",
+    "embeddings_model": "embeddings_model_id",
+    "webhook": None,
+    "track_id": None
+    })
+
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.json()['output'][0]
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from api import make_query
+from api import get_main_text, text_to_SD
 
-make_query("Why is the sky blue?")
+filename = 'text.txt'
+def read_file_as_string(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = file.read()
+    return data
+
+prompt = get_main_text(read_file_as_string(filename))
+print(prompt)
+image_link = text_to_SD(prompt)
+print(image_link)
