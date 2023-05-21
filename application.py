02e5ba4ca8cb550e1462e47dc6e1a14fd9f00997
Victor Ivanov <vivanov@paradigmtesting.com>
2023-05-21 19:14:59 -0400

    folder buildy

--- application.py
+++ application.py
@@ -1,12 +1,1 @@
-from api import get_main_text, text_to_SD
-
-filename = 'test/text.txt'
-def read_file_as_string(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        data = file.read()
-    return data
-
-prompt = get_main_text(read_file_as_string(filename))
-print(prompt)
-image_link = text_to_SD(prompt)
-print(image_link)
+from flask import Flask, redirect, url_for, render_template, request, session, jsonify, current_app
